6c75aa0f56b46902ea037b316ba4f45a37a6a090
devillerstyp <53711739+devillerstyp@users.noreply.github.com>
2020-05-14 00:33:45 +0200

    Update FicheFilm.java

--- FicheFilm.java
+++ FicheFilm.java
@@ -4,12 +4,15 @@ public class FicheFilm {
 
 
 		public static void main(String[] args) {
-		// Déclaration des variables
+		// DÃ©claration des variables
 		
 		String TitreFilm = "Indiana Jones et la derniere croisade";
 		boolean StatusFilm = true;
 		int AnneeDeSortie = 1987;
 		double NoteFilm = 8.2;
+			
+		// Affichage des variables
+			
 		System.out.println(TitreFilm);
 		System.out.println(StatusFilm);
 		System.out.println(AnneeDeSortie);
