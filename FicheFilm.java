d6247295e9b5845f151ee594f4fe88eb044cacfb
devillerstyp <53711739+devillerstyp@users.noreply.github.com>
2020-05-14 00:32:07 +0200

    Add files via upload

---
+++ FicheFilm.java
@@ -0,0 +1,20 @@
+package candies;
+
+public class FicheFilm {
+
+
+		public static void main(String[] args) {
+		// Déclaration des variables
+		
+		String TitreFilm = "Indiana Jones et la derniere croisade";
+		boolean StatusFilm = true;
+		int AnneeDeSortie = 1987;
+		double NoteFilm = 8.2;
+		System.out.println(TitreFilm);
+		System.out.println(StatusFilm);
+		System.out.println(AnneeDeSortie);
+		System.out.println(NoteFilm );
+		
+	}
+
+}
