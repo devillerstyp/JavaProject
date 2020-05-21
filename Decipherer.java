b582e0043ba821c76706e49ee8ee977660e8342f
devillerstyp <53711739+devillerstyp@users.noreply.github.com>
2020-05-21 11:08:32 +0200

    Add files via upload

--- Decipherer.java
+++ Decipherer.java
@@ -2,46 +2,19 @@ package Decipherer;
 
 public class Decipherer {
 
-    public static String dechiffrage(String message) {
-        if (message.equals("0@sn9sirppa@#?ia'jgtvryko1")) {
-            String message1 =  "0@sn9sirppa@#?ia'jgtvryko1";
-            // Calcul de la longueur
-            int mess1 = (message1.length())/2;
-            // recuperation sous chaine
-            String sbmessage1 = message1.substring (6, mess1);
-            // remplacer les caracteres par l'espace
-            String str1 = sbmessage1.replaceAll("@#?", "");
-            // inverser la chaîne
-            StringBuilder lettersBuff1 = new StringBuilder(str1);
-            String str_inverse1 = lettersBuff1.reverse().toString();
-            return str_inverse1;
+    public static String dechiffrage(String message){
 
-        }
-        if (message.equals("q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj")) {
-            String message2 =  "q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
-            int mess2 = (message2.length())/2;
-            String sbmessage2 = message2.substring (6, mess2);
-
-            String str2 = sbmessage2.replace("@#?", " ");
-
-            StringBuilder lettersBuff2 = new StringBuilder(str2);
-            String str_inverse2 = lettersBuff2.reverse().toString();
-            return str_inverse2;
-
-        }
-        if (message.equals("aopi?sedohtém@#?sedhtmg+p9l!")) {
-            String message3 =  "aopi?sedohtém@#?sedhtmg+p9l!";
-            int mess3 = (message3.length())/2;
-            String sbmessage2 = message3.substring (6, mess3);
-
-            String str3 = sbmessage2.replaceAll("@#?", " ");
-
-            StringBuilder lettersBuff3 = new StringBuilder(str3);
-            String str_inverse3 = lettersBuff3.reverse().toString();
-            return str_inverse3;
-
-        }
-        return message;
+        //String message1 =  "0@sn9sirppa@#?ia'jgtvryko1";
+        // Calcul de la longueur
+        int mess1 = (message.length())/2;
+        // recuperation sous chaine
+        String sbmessage = message.substring (5, 5+mess1);
+        // remplacer les caracteres par l'espace
+        String str = sbmessage.replace("@#?", " ");
+        // inverser la chaîne
+        StringBuilder lettersBuff = new StringBuilder(str);
+        String str_inverse = lettersBuff.reverse().toString();
+        return str_inverse;
 
     }
 
@@ -56,9 +29,7 @@ public class Decipherer {
         System.out.println (dechiffrage(message3));
 
          */
-        System.out.println (dechiffrage(message1) + dechiffrage(message2) + " " +dechiffrage(message3));
+        System.out.println (dechiffrage(message1)+ " " + dechiffrage(message2) + " " +dechiffrage(message3));
 
     }
-
-
 }
